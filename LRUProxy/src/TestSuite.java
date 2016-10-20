81c1c4a2081ba770f09ce05f9570ed3ebee89253
SugarXD <kirbyowens16@yahoo.com>
2016-10-20 12:45:33 -0400

    Created CacheLogTest file and testdata directory. Created setup & destroy methods.

---
+++ LRUProxy/src/TestSuite.java
@@ -0,0 +1,19 @@
+/**
+ * Created by TacoMeemert on 10/16/16.
+ */
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+       CacheLogTest.class, //test case 1
+})
+
+public class TestSuite {
+}
+
+
+
+
