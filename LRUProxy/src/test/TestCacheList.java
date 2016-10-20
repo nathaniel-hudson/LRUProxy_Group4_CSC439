a4f211e6227c1d2bffb0583ec7b00b51272a9a3b
Katie Schwegman <cmschwegman@yahoo.com>
2016-10-20 18:54:13 -0400

    Added TestCacheList class to test folder

--- LRUProxy/src/test/TestCacheList.java
+++ LRUProxy/src/test/TestCacheList.java
@@ -18,6 +18,9 @@ public class TestCacheList {
     private static CacheList emptylist;
     private static int size = 5;
 
+    /**
+     * Initializes the CacheLists and adds things to the non-empty one
+     */
     @BeforeClass
     public static void beforeClass() {
         list = new CacheList("./", size);
@@ -31,30 +34,47 @@ public class TestCacheList {
         emptylist = new CacheList("./", 1);
     }
 
+    /**
+     * Tests adding a new object to empty list using addNewObject method of CacheList class.
+     * This should return an empty string.
+     */
     @Test
     public void addNewObjectTestMiss(){
         System.out.println("Testing if add miss works");
         assertEquals("", emptylist.addNewObject("www.yahoo.com", false));
     }
 
+    /**
+     * Tests adding a new object to a full list using addNewObject method of CacheList class.
+     * This should return a string of the object that was removed ("www.yahoo.com" in this case).
+     */
     @Test
     public void addNewObjectRemoval(){
         System.out.println("Testing if adding with removal works");
         assertEquals("www.yahoo.com", emptylist.addNewObject("www.google.com", false));
     }
 
+    /**
+     * Tests getting the Cache size.
+     */
     @Test
     public void getCacheSizeTest(){
         System.out.println("Testing if getting size works");
         assertEquals(size, list.getCacheSize());
     }
 
+    /**
+     * Tests getting the head of the CacheList.
+     */
     @Test
     public void getHeadTest(){
         System.out.println("Testing if get head works");
         assertEquals("www.google.com", emptylist.getHead());
     }
 
+    /**
+     * Tests the get function of the CacheList.
+     */
     @Test
     public void getTest(){
         System.out.println("Testing if get works");
